5254e2f527af681c99ead383786e568506808ecd
iamtanzim <tanzim67@gmail.com>
2020-10-11 19:47:01 +0600

    2-Set up Vue Js Application

--- bookingsite/resources/js/app.js
+++ bookingsite/resources/js/app.js
@@ -1,1 +1,8 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+Vue.component('example-component', require('./component/ExampleComponent.vue').default);
+const app = new Vue({
+    el: '#app',
+});
+
