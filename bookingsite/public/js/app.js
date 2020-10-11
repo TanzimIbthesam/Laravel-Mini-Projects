3c1449d2a6fafafc8bae93adc08c35312262eec1
iamtanzim <tanzim67@gmail.com>
2020-10-11 21:29:36 +0600

    v-bind,v-shorthand value and named routes

--- bookingsite/public/js/app.js
+++ bookingsite/public/js/app.js
@@ -20441,7 +20441,11 @@ var render = function() {
                 _c(
                   "li",
                   { staticClass: "nav-item active" },
-                  [_c("router-link", { attrs: { to: "/" } }, [_vm._v("Home")])],
+                  [
+                    _c("router-link", { attrs: { to: { name: "home" } } }, [
+                      _vm._v("Home")
+                    ])
+                  ],
                   1
                 ),
                 _vm._v(" "),
