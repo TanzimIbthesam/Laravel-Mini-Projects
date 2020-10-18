416c82a1c19e6032c883c8b9d7f6f9ff10950373
iamtanzim <tanzim67@gmail.com>
2020-10-18 17:34:04 +0600

    50-Booking Model and Relations

--- bookingsite/app/Models/Booking.php
+++ bookingsite/app/Models/Booking.php
@@ -5,13 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Bookable extends Model
+class Booking extends Model
 {
-
     use HasFactory;
-    public function bookable()
+    public function booking()
     {
-        return $this->hasMany(Booking::class);
+        return $this->belongsTo(Bookable::class);
     }
-
 }
