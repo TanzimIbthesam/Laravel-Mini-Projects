5fcb6b2a44e6337c9b3bfff2be9e908987c718fb
iamtanzim <tanzim67@gmail.com>
2020-10-19 10:06:19 +0600

    54-Eloquent Query Scopes

--- bookingsite/app/Http/Controllers/Api/BookableAvailabilityController.php
+++ bookingsite/app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -16,6 +16,11 @@ class BookableAvailabilityController extends Controller
     public function __invoke(Request $request)
     {
         //
-        dd('Hello world');
-    }
+        $data = $request->validate([
+            'from' => 'required|date_format:Y-m-d|after_or_equal:now',
+            'to' => 'required|date_format:Y-m-d|after_or_equal:from'
+        ]);
+
+        dd($data);
+}
 }
