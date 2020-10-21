b76410c02649af9dbbad0ac503f04437b309a012
iamtanzim <tanzim67@gmail.com>
2020-10-21 12:37:38 +0600

    60-Review Model and migration(using uuid as model primary key)

---
+++ bookingsite/database/migrations/2020_10_21_062527_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->timestamps();
+           $table->unsignedTinyInteger('rating');
+           $table->text('content');
+            $table->foreignId('bookable_id')->constrained('bookables')->nullable();
+            $table->foreignId('booking_id')->constrained('bookings');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
